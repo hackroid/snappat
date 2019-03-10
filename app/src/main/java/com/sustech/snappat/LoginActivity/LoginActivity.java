8210e920af71d2983d4e96d243216fb5fe2abb1f
Snooowgh <sustechapp@qq.com>
2019-03-10 22:47:36 +0800

    Create Camera & Login Activity

---
+++ app/src/main/java/com/sustech/snappat/LoginActivity/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.sustech.snappat.LoginActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.sustech.snappat.CameraActivity.CameraActivity;
+import com.sustech.snappat.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button button = (Button) findViewById(R.id.login_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getBaseContext().getSharedPreferences(getString(R.string.app_preference), Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean(getString(R.string.pref_login),true);
+                editor.commit();
+                Toast.makeText(LoginActivity.this,
+                        "Login Sucessfully", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+}
