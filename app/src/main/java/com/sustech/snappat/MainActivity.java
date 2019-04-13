792fd53752d1997c8ca32b3416ce844514992013
sheepmanchen <sheepman314@gmail.com>
2019-04-13 16:40:17 +0800

    initial version

--- app/src/main/java/com/sustech/snappat/MainActivity.java
+++ app/src/main/java/com/sustech/snappat/MainActivity.java
@@ -1,22 +1,11 @@
 package com.sustech.snappat;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import com.sustech.snappat.CameraActivity.CameraActivity;
-import com.sustech.snappat.LoginActivity.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,34 +39,6 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-//        Button used for test
-        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.container);
-        final Context context = this;
-        Button camera_button = new Button(getBaseContext());
-        camera_button.setText("CameraButton");
-        camera_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SharedPreferences sharedPreferences = getBaseContext().getSharedPreferences(getString(R.string.app_preference), Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putBoolean(getString(R.string.pref_login),false);
-                editor.commit();
-                Intent camera_intent = new Intent(context, CameraActivity.class);
-                startActivity(camera_intent );
-            }
-        });
-        constraintLayout.addView(camera_button);
-
-
-        SharedPreferences sharedPreferences = getBaseContext().getSharedPreferences(getString(R.string.app_preference), Context.MODE_PRIVATE);
-        boolean islogin = sharedPreferences.getBoolean(getString(R.string.pref_login), false);
-        if (!islogin){
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        } else {
-//            TODO: load main screen content
-        }
     }
 
 }
