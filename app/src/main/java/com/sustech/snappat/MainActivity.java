337e0e1a738809341cd8939fd3afc66e252ced3c
RenTao <623059008@qq.com>
2019-04-14 19:21:18 +0800

    添加了strings里的内容，修改了menu，使bottomMenu更好看了

--- app/src/main/java/com/sustech/snappat/MainActivity.java
+++ app/src/main/java/com/sustech/snappat/MainActivity.java
@@ -18,9 +18,9 @@ import android.widget.TextView;
 import com.sustech.snappat.CameraActivity.CameraActivity;
 import com.sustech.snappat.LoginActivity.LoginActivity;
 
-public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+
+public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener  = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -28,13 +28,12 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_cam:
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_message:
+                    return true;
+                case R.id.navigation_person:
                     return true;
             }
             return false;
@@ -46,8 +45,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         //Button used for test
@@ -78,5 +76,6 @@ public class MainActivity extends AppCompatActivity {
             //TODO: load main screen content
         }
     }
+
 
 }
